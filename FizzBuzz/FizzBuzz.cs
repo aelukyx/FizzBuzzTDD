6e855447571b905ec31ee69aa76304db6687a036
Alexander Raúl Cabrera Guevara <arcabrerag@outlook.com>
2014-10-11 14:32:35 -0500

    M

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public String IsFizzOrBuzz(int num)
+        {
+            var solution = "";
+
+            if (num == 1)
+            {
+                solution = Convert.ToString(num);
+
+            }
+
+            return solution;
+        }
+
+
+    }
+}
---
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using FizzBuzz;
+
+namespace FizzBuzzTest
+{
+
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        private FizzBuzz.FizzBuzz fizzBuzz { get; set; }
+
+        [SetUp]
+        public void InstaciasAntesDeCadaTest()
+        {
+            // Arrange
+            fizzBuzz = new FizzBuzz.FizzBuzz();
+        }
+
+        [Test]
+        public void TestNumero1()
+        {
+
+            // Act
+            var num = fizzBuzz.IsFizzOrBuzz(1);
+
+            // Assert
+            Assert.AreEqual("1", num);
+
+        }
+    }
+
+}
